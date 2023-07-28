921a31a2fc8b62235e42828d94ea271c581ebaf3
INTE/MG/0925/09 <donyimbo@kabarak.ac.ke>
2023-07-28 14:23:34 +0300

    FINAL UPDATE

--- database/homeverse.sql
+++ database/homeverse.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Jul 09, 2023 at 11:57 AM
--- Server version: 8.0.31
--- PHP Version: 8.0.26
+-- Host: localhost
+-- Generation Time: Jul 28, 2023 at 01:20 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -27,28 +27,26 @@ SET time_zone = "+00:00";
 -- Table structure for table `orders`
 --
 
-DROP TABLE IF EXISTS `orders`;
-CREATE TABLE IF NOT EXISTS `orders` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `backgroundName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `propertyName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL,
+  `backgroundName` varchar(255) NOT NULL,
+  `propertyName` varchar(255) NOT NULL,
   `propertyPrice` decimal(10,2) NOT NULL,
-  `status` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `full_name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `phone` int NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `status` varchar(255) NOT NULL,
+  `full_name` varchar(255) NOT NULL,
+  `phone` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `orders`
 --
 
 INSERT INTO `orders` (`id`, `backgroundName`, `propertyName`, `propertyPrice`, `status`, `full_name`, `phone`, `email`) VALUES
-(8, 'Background-Name-6448.jpg', 'Germane Flynn', '59200.00', 'Cancelled', 'Miles Morales', 712545335, 'milesmorales@mailinator.com'),
-(9, 'Background-Name-9370.jpg', 'Adrienne Villa', '57000.00', 'Bought', 'Mr Shelby', 754060142, 'shelbyreals@hotmail.com'),
-(10, 'Background-Name-2263.jpg', 'Yael Flats', '29500.00', 'Ordered', 'Clyde Artz', 740965535, 'clydeartz@mailinator.com'),
-(11, 'Background-Name-6448.jpg', 'Germane Flynn', '59200.00', 'Bought', 'Chris Baaru', 714203994, 'baaru@mailinator.com');
+(8, 'Background-Name-6448.jpg', 'Germane Flynn', 59200.00, 'Cancelled', 'Miles Morales', 712545335, 'milesmorales@mailinator.com'),
+(9, 'Background-Name-9370.jpg', 'Adrienne Villa', 57000.00, 'Bought', 'Mr Shelby', 754060142, 'shelbyreals@hotmail.com'),
+(10, 'Background-Name-2263.jpg', 'Yael Flats', 29500.00, 'Ordered', 'Clyde Artz', 740965535, 'clydeartz@mailinator.com'),
+(12, 'Background-Name-6448.jpg', 'Germane Flynn', 59200.00, 'Bought', 'Faith Katelina Kilonzo', 115687568, 'reginarobert355@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -56,50 +54,48 @@ INSERT INTO `orders` (`id`, `backgroundName`, `propertyName`, `propertyPrice`, `
 -- Table structure for table `property`
 --
 
-DROP TABLE IF EXISTS `property`;
-CREATE TABLE IF NOT EXISTS `property` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `type` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `status` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `city` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `backgroundName` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `pictureName` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `pictureName2` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+CREATE TABLE `property` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `backgroundName` varchar(255) NOT NULL,
+  `pictureName` varchar(255) NOT NULL,
+  `pictureName2` varchar(255) NOT NULL,
   `oldPrice` decimal(10,2) NOT NULL,
   `discount` decimal(10,2) NOT NULL,
   `price` decimal(10,2) NOT NULL,
-  `discountPercent` int NOT NULL,
-  `bedrooms` int NOT NULL,
-  `bathrooms` int NOT NULL,
-  `squareFt` int NOT NULL,
-  `description` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `featured` varchar(10) COLLATE utf8mb4_general_ci NOT NULL,
-  `active` varchar(10) COLLATE utf8mb4_general_ci NOT NULL,
-  `owner` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `phone` int NOT NULL,
-  `whatsapp` int NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `blog` varchar(10) COLLATE utf8mb4_general_ci NOT NULL,
-  `comment` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `featured_blog` varchar(10) COLLATE utf8mb4_general_ci NOT NULL,
-  `active_blog` varchar(10) COLLATE utf8mb4_general_ci NOT NULL,
-  `date` date NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `discountPercent` int(11) NOT NULL,
+  `bedrooms` int(11) NOT NULL,
+  `bathrooms` int(11) NOT NULL,
+  `squareFt` int(11) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `featured` varchar(10) NOT NULL,
+  `active` varchar(10) NOT NULL,
+  `duration` varchar(255) NOT NULL,
+  `owner` varchar(255) NOT NULL,
+  `phone` int(11) NOT NULL,
+  `whatsapp` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `blog` varchar(10) NOT NULL,
+  `comment` varchar(255) NOT NULL,
+  `featured_blog` varchar(10) NOT NULL,
+  `active_blog` varchar(10) NOT NULL,
+  `dates` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `property`
 --
 
-INSERT INTO `property` (`id`, `name`, `type`, `status`, `city`, `backgroundName`, `pictureName`, `pictureName2`, `oldPrice`, `discount`, `price`, `discountPercent`, `bedrooms`, `bathrooms`, `squareFt`, `description`, `featured`, `active`, `owner`, `phone`, `whatsapp`, `email`, `blog`, `comment`, `featured_blog`, `active_blog`, `date`) VALUES
-(27, 'Artkins Key House', 'House', 'For Rent', 'Nakuru', 'Background-Name-5041.jpg', 'Picture-Name-5606.jpg', 'Picture-Name2-2508.jpg', '50000.00', '500.00', '49500.00', 1, 10, 5, 3652, 'Comfortable House for Family Vacation', 'Yes', 'Yes', 'Artkins', 757451020, 757451020, 'artkins@hotmail.com', 'Yes', 'Best House of 2022', 'Yes', 'Yes', '2022-06-09'),
-(28, 'Artcorp Complex', 'Office', 'For Sale', 'Nairobi', 'Background-Name-3586.jpeg', 'Picture-Name-6363.jpg', 'Picture-Name2-4863.jpg', '100000.00', '1000.00', '99000.00', 1, 0, 100, 5065, 'Luxury 5Star Office', 'Yes', 'Yes', 'Whitney', 714230692, 714230692, 'whitneyartkins@hotmail.com', 'Yes', 'Most Luxurios Office of 2022', 'No', 'Yes', '2022-01-02'),
-(29, 'Wilkinson Apartments', 'Apartment', 'For Rent', 'Naivasha', 'Background-Name-6653.jpg', 'Picture-Name-2126.jpg', 'Picture-Name2-5944.jpg', '9000.00', '1000.00', '8000.00', 11, 1, 1, 3280, 'Cool Apartments for Students', 'Yes', 'Yes', 'Shaun', 783784054, 783784054, 'shaunartkins@hotmail.com', 'Yes', 'Cheapest Apartments of 2021', 'Yes', 'Yes', '2021-09-16'),
-(30, 'Franklin Holdings', 'Building', 'For Sale', 'Mombasa', 'Background-Name-9170.jpg', 'Picture-Name-6386.jpg', 'Picture-Name2-9139.jpg', '50000.00', '2000.00', '48000.00', 4, 3, 5, 2045, 'Unique Building for Business', 'Yes', 'Yes', 'Franklin', 783784054, 783784054, 'franklinholdings@gmailcom', 'No', '', '', '', '0000-00-00'),
-(31, 'Yael Flats', 'Flat', 'For Rent', 'Kisumu', 'Background-Name-2263.jpg', 'Picture-Name-698.jpg', 'Picture-Name2-9287.jpg', '30000.00', '500.00', '29500.00', 2, 8, 4, 3524, 'Luxurious Flats for Family', 'Yes', 'Yes', 'Livia', 714230692, 714230692, 'liviaartkins@hotmail.com', 'Yes', 'Coolest Flats of 2023', 'No', 'Yes', '2023-02-09'),
-(32, 'Adrienne Villa', 'Villa', 'For Sale', 'Nakuru', 'Background-Name-9370.jpg', 'Picture-Name-7928.jpg', 'Picture-Name2-1095.jpg', '60000.00', '3000.00', '57000.00', 5, 10, 5, 3542, 'Comfortable Villa', 'Yes', 'Yes', 'Miles', 720421570, 714230692, 'milesmorales@mailinator.com', 'No', '', '', '', '0000-00-00'),
-(33, 'Germane Flynn', 'House', 'For Rent', 'Nairobi', 'Background-Name-6448.jpg', 'Picture-Name-3412.jpg', 'Picture-Name2-7547.jpg', '60000.00', '800.00', '59200.00', 1, 12, 5, 3540, 'Luxurious House', 'Yes', 'Yes', 'Whitney', 712545335, 712545335, 'whitneyartkins@hotmail.com', 'Yes', 'Most Luxurious House I have ever seen', 'Yes', 'Yes', '2023-01-09');
+INSERT INTO `property` (`id`, `name`, `type`, `status`, `city`, `backgroundName`, `pictureName`, `pictureName2`, `oldPrice`, `discount`, `price`, `discountPercent`, `bedrooms`, `bathrooms`, `squareFt`, `description`, `featured`, `active`, `duration`, `owner`, `phone`, `whatsapp`, `email`, `blog`, `comment`, `featured_blog`, `active_blog`, `dates`) VALUES
+(27, 'Artkins Key House', 'House', 'For Rent', 'Nakuru', 'Background-Name-5041.jpg', 'Picture-Name-5606.jpg', 'Picture-Name2-2508.jpg', 50000.00, 500.00, 49500.00, 1, 10, 5, 3652, 'Comfortable House for Family Vacation', 'Yes', 'Yes', '/Month', 'Artkins', 757451020, 757451020, 'artkins@hotmail.com', 'Yes', 'Best House of 2022', 'Yes', 'Yes', '2022-06-09'),
+(29, 'Wilkinson Apartments', 'Apartment', 'For Rent', 'Naivasha', 'Background-Name-6653.jpg', 'Picture-Name-2126.jpg', 'Picture-Name2-5944.jpg', 9000.00, 1000.00, 8000.00, 11, 1, 1, 3280, 'Cool Apartments for Students', 'Yes', 'Yes', '/Month', 'Shaun', 783784054, 783784054, 'shaunartkins@hotmail.com', 'Yes', 'Cheapest Apartments of 2021', 'Yes', 'Yes', '2021-09-16'),
+(30, 'Franklin Holdings', 'Building', 'For Sale', 'Mombasa', 'Background-Name-9170.jpg', 'Picture-Name-6386.jpg', 'Picture-Name2-9139.jpg', 50000.00, 2000.00, 48000.00, 4, 3, 5, 2045, 'Unique Building for Business', 'Yes', 'Yes', '/Month', 'Franklin', 783784054, 783784054, 'franklinholdings@gmailcom', 'No', '', '', '', '0000-00-00'),
+(31, 'Yael Flats', 'Flat', 'For Rent', 'Kisumu', 'Background-Name-2263.jpg', 'Picture-Name-698.jpg', 'Picture-Name2-9287.jpg', 30000.00, 500.00, 29500.00, 2, 8, 4, 3524, 'Luxurious Flats for Family', 'Yes', 'Yes', '/Month', 'Livia', 714230692, 714230692, 'liviaartkins@hotmail.com', 'Yes', 'Coolest Flats of 2023', 'No', 'Yes', '2023-02-09'),
+(32, 'Adrienne Villa', 'Villa', 'For Sale', 'Nakuru', 'Background-Name-9370.jpg', 'Picture-Name-7928.jpg', 'Picture-Name2-1095.jpg', 60000.00, 3000.00, 57000.00, 5, 10, 5, 3542, 'Comfortable Villa', 'Yes', 'Yes', '/Month', 'Miles', 720421570, 714230692, 'milesmorales@mailinator.com', 'No', '', '', '', '0000-00-00'),
+(33, 'Germane Flynn', 'House', 'For Rent', 'Nairobi', 'Background-Name-6448.jpg', 'Picture-Name-3412.jpg', 'Picture-Name2-7547.jpg', 60000.00, 800.00, 59200.00, 1, 12, 5, 3540, 'Luxurious House', 'Yes', 'Yes', '/Month', 'Whitney', 712545335, 712545335, 'whitneyartkins@hotmail.com', 'Yes', 'Coolest House of 2022', 'Yes', 'Yes', '2022-02-22');
 
 -- --------------------------------------------------------
 
@@ -107,26 +103,22 @@ INSERT INTO `property` (`id`, `name`, `type`, `status`, `city`, `backgroundName`
 -- Table structure for table `reviews`
 --
 
-DROP TABLE IF EXISTS `reviews`;
-CREATE TABLE IF NOT EXISTS `reviews` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `property_id` int NOT NULL,
-  `username` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `profileName` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `review` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `active` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `reviews` (
+  `id` int(11) NOT NULL,
+  `property_id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `review` varchar(255) NOT NULL,
+  `active` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `reviews`
 --
 
-INSERT INTO `reviews` (`id`, `property_id`, `username`, `profileName`, `review`, `active`) VALUES
-(30, 33, 'Miles Morales', 'Profile-Name-3435.jpeg', 'Best House. I like it. Thanks Homeverse', 'Yes'),
-(31, 32, 'Mr Shelby', 'Profile-Name-1441.jpeg', 'This is the best place to feel at home. Coolest Vila have ever lived in.', 'Yes'),
-(32, 31, 'Clyde Artz', 'Profile-Name-3730.jpg', 'Yea man I really like this flat. Thanks homverse', 'Yes'),
-(33, 33, 'Baaru', 'Profile-Name-4332.jpg', 'Most Luxurious House I have ever seen', 'Yes');
+INSERT INTO `reviews` (`id`, `property_id`, `username`, `review`, `active`) VALUES
+(37, 33, 'Whitney', 'This is the most spacious house i have seen so far', 'Yes'),
+(38, 33, 'Artkins', 'This is the best house i have bought', 'Yes'),
+(39, 33, 'Livia', 'Get your dream house here at Homeverse', 'Yes');
 
 -- --------------------------------------------------------
 
@@ -134,30 +126,87 @@ INSERT INTO `reviews` (`id`, `property_id`, `username`, `profileName`, `review`,
 -- Table structure for table `user`
 --
 
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE IF NOT EXISTS `user` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `full_name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `username` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `userProfile` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `phone` int NOT NULL,
-  `password` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `admin` varchar(10) COLLATE utf8mb4_general_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `full_name` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `userProfile` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone` int(11) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `admin` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id`, `full_name`, `username`, `userProfile`, `email`, `phone`, `password`, `admin`) VALUES
-(1, 'Don Artkins', 'Artkins', 'User-Profile-8462.jpg', 'bellachao@hotmail.com', 757451020, '21232f297a57a5a743894a0e4a801fc3', 'Yes'),
-(2, 'Whitney Artkins', 'Whitney', 'User-Profile-8462.jpg', 'whitney254@hotmail.com', 757451020, '21232f297a57a5a743894a0e4a801fc3', 'Yes'),
-(9, 'Emmanuel Oketch', 'Manu', 'User-Profile-8462.jpg', 'manu@hotmail.com', 729227872, '9de4a97425678c5b1288aa70c1669a64', 'No'),
-(17, 'Chris Baaru', 'Baaru', 'User-Profile-8262.jpeg', 'baaru@mailinator.com', 712545335, 'a765884f4d99cc66fb7e2abc9ffe4156', 'No'),
-(19, 'Livia Artkins', 'Livia', 'User-Profile-1602.jpg', 'liviaartkins@hotmail.com', 757451020, 'd6a6bc0db10694a2d90e3a69648f3a03', 'No'),
-(20, 'Shaun Artkins', 'Shaun', '', 'shaunartkins@hotmail.com', 712545335, 'd6a6bc0db10694a2d90e3a69648f3a03', 'No');
+(1, 'Don Artkins', 'Artkins', 'User-Profile-2849.jpg', 'bellachao@hotmail.com', 757451020, '21232f297a57a5a743894a0e4a801fc3', 'Yes'),
+(2, 'Whitney Artkins', 'Whitney', 'User-Profile-2828.png', 'whitney254@hotmail.com', 757451020, '21232f297a57a5a743894a0e4a801fc3', 'Yes'),
+(19, 'Livia Artkins', 'Livia', 'User-Profile-4414.jpg', 'liviaartkins@hotmail.com', 757451020, 'd6a6bc0db10694a2d90e3a69648f3a03', 'No'),
+(20, 'Shaun Artkins', 'Shaun', 'User-Profile-3156.jpg', 'shaunartkins@hotmail.com', 712545335, 'd6a6bc0db10694a2d90e3a69648f3a03', 'No'),
+(24, 'Roanna Rush', 'Rush', 'User-Profile-6752.jpeg', 'xeqix@mailinator.com', 712663975, '47982c18f4861b2edf96bfe9f73f12bf', 'No'),
+(27, 'Chris Baaru', 'Baaru', 'No-Profile.png', 'chrisbaaru@gmail.com', 114663795, 'a765884f4d99cc66fb7e2abc9ffe4156', 'No'),
+(28, 'Oketch Manu', 'Manu', 'No-Profile.png', 'oketchmanu@hotmail.com', 114291362, 'a765884f4d99cc66fb7e2abc9ffe4156', 'No'),
+(29, 'Faith Katelina Kilonzo', 'Katelina', 'No-Profile.png', 'reginarobert355@gmail.com', 115687568, '21232f297a57a5a743894a0e4a801fc3', 'Yes'),
+(37, 'Ben Carson', 'Ben', 'No-Profile.png', 'bencarson@mailinator.com', 745412554, '7fe4771c008a22eb763df47d19e2c6aa', 'No');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `property`
+--
+ALTER TABLE `property`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `reviews`
+--
+ALTER TABLE `reviews`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `property`
+--
+ALTER TABLE `property`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+
+--
+-- AUTO_INCREMENT for table `reviews`
+--
+ALTER TABLE `reviews`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
